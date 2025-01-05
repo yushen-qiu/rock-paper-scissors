c463c469e8951da1aef9ea19b3d3fd08b8e51801
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-01-05 13:02:18 +1100

    Create function that plays a single round
    Function playRound gets the human and computer choice as parameters
    Depending on the inputs it will output a win/tie/lose message
    It will also change the global variables for score accordingly

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function getHumanChoice() {
     let choice = prompt("Please choose rock or paper or scissors")
     return choice
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if ((humanChoice === "rock" && computerChoice === "scissors") 
+        || (humanChoice === "paper" && computerChoice === "rock")
+        || (humanChoice === "scissors" && computerChoice === "paper")) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+            humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
