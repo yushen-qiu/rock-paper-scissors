f7041b5e6e02503b4d05fa15fad2e5a08f532f05
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-01-05 12:47:44 +1100

    Create function to randomly return computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let n = Math.floor(Math.random() * 3)
+    let choice
+    if (n === 0) {
+        choice = "rock"
+    } else if (n === 1) {
+        choice = "paper"
+    } else if (n === 2) {
+        choice = "scissors"
+    }
+    return choice
+}
