a72a3c0c2e1e4d55f6cdf33bab0cfeeb1604c866
Daniel Qiu <61307453+yushen-qiu@users.noreply.github.com>
2025-01-05 13:16:30 +1100

    Create function to play a game of 5 rounds
    Score variables and playRound function were moved into playGame
    For loop to loop 5 times to play 5 rounds
    if else block to find the winner/loser/tie

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0
-let computerScore = 0
-
 function getComputerChoice() {
     let n = Math.floor(Math.random() * 3)
     let choice
@@ -19,23 +16,42 @@ function getHumanChoice() {
     return choice
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase()
-    
-    if (humanChoice === computerChoice) {
-        console.log("It's a tie!")
-    } else if ((humanChoice === "rock" && computerChoice === "scissors") 
-        || (humanChoice === "paper" && computerChoice === "rock")
-        || (humanChoice === "scissors" && computerChoice === "paper")) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-            humanScore++;
+
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase()
+        
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie!")
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") 
+            || (humanChoice === "paper" && computerChoice === "rock")
+            || (humanChoice === "scissors" && computerChoice === "paper")) {
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            computerScore++;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You are the winner! You won ${humanScore}/5 games.`)
+    } else if (humanScore < computerScore) {
+        console.log(`You are the loser! The computer won ${computerScore}/5 games.`)
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
-        computerScore++;
+        console.log(`You tied with the computer!`)
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+playGame()
 
-playRound(humanSelection, computerSelection);
